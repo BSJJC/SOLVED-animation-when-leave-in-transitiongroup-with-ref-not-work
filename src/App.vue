6acc34a12e0a5b69a4fef3de6f1dc950a874a57b
BSJJC <805086021@qq.com>
2024-04-08 23:15:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <TransitionGroup name="container" tag="ul" class="container">
+    <div v-for="(_, index) in counts" :key="index">
+      {{ index }}
+
+      <!-- animation when leave doesn't work -->
+      <input ref="inputRef" />
+
+      <!-- animation when leave works -->
+      <!-- <input /> -->
+
+      <button @click="remove(index)">delete</button>
+    </div>
+
+    <button @click="add">add input</button>
+  </TransitionGroup>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counts = ref([1, 2, 3, 4, 5]);
+const inputRef = ref();
+
+const add = () => {
+  counts.value.push(counts.value[counts.value.length - 1] + 1);
+};
+
+const remove = (index) => {
+  counts.value.splice(index, 1);
+};
+</script>
+
+<style>
+.container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  row-gap: 2rem;
+  background: #add8e620;
+}
+
+.container-move,
+.container-enter-active,
+.container-leave-active {
+  transition: all 0.3s ease-in-out;
+}
+
+.container-enter-from,
+.container-leave-to {
+  opacity: 0;
+  transform: translateX(50px);
+}
+
+.container-leave-active {
+  position: absolute;
+}
+</style>
